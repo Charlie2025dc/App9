a499851aef8cd80aa98d676e01b6986e4ad9c6d8
Charlie2025dc <carlos.aguilar.perez.cbtis16@gmail.com>
2017-05-24 13:31:34 -0500

    3

--- js/acciones.js
+++ js/acciones.js
@@ -30,7 +30,7 @@ function getPosition () {
 		'Altitude Accuracy:' + position.coords.altitudeAccuracy +'\n'+ 
 		'Heading:' + position.coords.heading +'\n'+ 
 		'Speed:' + position.coords.speed +'\n'+ 
-		'Timestamp:' + position.coords.timestamp +'\n');
+		'Timestamp:' + position.timestamp +'\n');
 	}
  function onError(error) {
 	 alert('code' + error.code + '\n' + 'message:' + error.message + '\n');
@@ -54,7 +54,7 @@ function watchPosition() {
 		$('#aaccuracy').html(position.coords.altitudeAccuracy);
 		$('#heading').html(position.coords.heading);
 		$('#speed').html(position.coords.speed);
-		$('#timestamp').html(position.coords.timestamp);
+		$('#timestamp').html(position.timestamp);
 	};
 	function OnError(error) {
 		 alert('code' + error.code + '\n' + 'message:' + error.message + '\n');
